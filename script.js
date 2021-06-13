fb0dc3f35670bd11a7e84c58f63e3c27d0717da9
Vladimir <woyaxx@gmail.com>
2021-06-13 20:58:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+  $('.slider').slick({
+  });
+  $('.insta').slick({
+  infinite: true,
+  slidesToShow: 6,
+  infinite: true,
+   arrows: false,
+  slidesToScroll: 3
+});
+});
